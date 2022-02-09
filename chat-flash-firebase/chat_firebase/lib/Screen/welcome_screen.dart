63c6d97ec61acf64f9e7a6a1c7ff5f2ded315e1f
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2022-02-09 22:42:24 +0800

    Controller Animation Flutter
    Persen text and Animation show print and make controller animation with sigletickerproviderstatemixin

--- chat-flash-firebase/chat_firebase/lib/Screen/welcome_screen.dart
+++ chat-flash-firebase/chat_firebase/lib/Screen/welcome_screen.dart
@@ -9,7 +9,29 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      upperBound: 100,
+    );
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {
+        
+      });
+      print(controller.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,15 +50,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   child: Container(
                     child: Image.asset('images/logo.png'),
                     height: 60.0,
-                  ), 
+                  ),
                 ),
                 Text(
-                  'Flash Chat',
+                  '${controller.value.toInt()}', //persen
                   style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.amber
-                  ),
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.amber),
                 ),
               ],
             ),
@@ -44,47 +65,47 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               height: 48.0,
             ),
             Hero(
-                tag: 'login',
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Material(
-                    elevation: 5.0,
-                    color: Colors.lightBlue,
-                    borderRadius: BorderRadius.circular(30.0),
-                    child: MaterialButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, LoginScreen.id);
-                      },
-                      minWidth: 200.0,
-                      height: 42.0,
-                      child: Text(
-                        'Log In',
-                      ),
+              tag: 'login',
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  elevation: 5.0,
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Log In',
                     ),
                   ),
                 ),
+              ),
             ),
-           Hero(
-               tag: 'register',
-               child:  Padding(
-                 padding: EdgeInsets.symmetric(vertical: 16.0),
-                 child: Material(
-                   color: Colors.lightBlue,
-                   borderRadius: BorderRadius.circular(30.0),
-                   elevation: 5.0,
-                   child: MaterialButton(
-                     onPressed: () {
-                       Navigator.pushNamed(context, RegistrationScreen.id);
-                     },
-                     minWidth: 200.0,
-                     height: 42.0,
-                     child: Text(
-                       'Register',
-                     ),
-                   ),
-                 ),
-               ),
-           )
+            Hero(
+              tag: 'register',
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(30.0),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RegistrationScreen.id);
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Register',
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
