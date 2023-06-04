0cb027ac397c76cd48ece4c0a249e267734d0c0e
Dwi Vicco <50991950+vicco7s@users.noreply.github.com>
2023-06-04 21:57:26 +0800

    remove

--- kec_app/lib/main.dart
+++ kec_app/lib/main.dart
@@ -2,8 +2,6 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kec_app/page/Pegawai/pegawaiASN.dart';
-import 'package:kec_app/page/Users/RegisterPage.dart';
 import 'package:kec_app/screen/SplashScreen.dart';
 
 // add firestore ke widget flutter
