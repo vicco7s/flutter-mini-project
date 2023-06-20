031b32c0dadfbec9c1e47a8140a1f08fe0132c76
Dwi Vicco <50991950+vicco7s@users.noreply.github.com>
2023-06-20 23:36:38 +0800

    commit Push Notification

--- kec_app/lib/main.dart
+++ kec_app/lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, unnecessary_const
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:kec_app/api/firebase_api.dart';
 import 'package:kec_app/screen/SplashScreen.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
@@ -10,6 +12,8 @@ import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Inisialisasi Firebase Messaging
+  await FirebaseApi().initNotifications();
   // Inisialisasi Firebase Storage
   await firebase_storage.FirebaseStorage.instance;
   runApp(SalbaApp());
